b6b6d33dc0cda33737b1280a816d9bc3cbdfdf51
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-06-20 09:37:12 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioMetAbstrato/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioMetAbstrato
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double height, width;
+            int qtt;
+
+            Console.Write("Digite a quantidade de figuras: ");
+            qtt = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ ExercicioMetAbstrato/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMetAbstrato
+{
+    abstract class Shape
+    {
+        Color color;
+
+        public double area()
+        {
+            return area();
+        }
+    }
+}

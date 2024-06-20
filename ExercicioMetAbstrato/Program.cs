61bb88fdd650b19d562ff32193f2fef0a55c317a
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-06-20 12:02:53 -0300

    Implementação da classe program

--- ExercicioMetAbstrato/Program.cs
+++ ExercicioMetAbstrato/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ExercicioMetAbstrato.Entities.Enums;
+using ExercicioMetAbstrato.Entities;
+using System.Globalization;
 
 namespace ExercicioMetAbstrato
 {
@@ -6,13 +9,46 @@ namespace ExercicioMetAbstrato
     {
         public static void Main(string[] args)
         {
-            double height, width;
             int qtt;
+
+            List<Shape> list = new List<Shape>();
 
             Console.Write("Digite a quantidade de figuras: ");
             qtt = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < qtt; i++) 
+            {
+                Console.WriteLine($"Shape #{i} data: ");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
 
+                Console.Write("Color (Black/Blue/Red): ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+
+                if(ch == 'r')
+                {
+                    Console.Write("width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Console.Write("heigth: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new Rectangle(width, height, color));
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Circle(radius, color));
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("SHAPE AREAS: ");
+            foreach (Shape shape in list)
+            {
+                Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
